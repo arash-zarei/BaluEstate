405b1e87f65f28562d8fad1cc1e10063b42b8a05
Arash Zarei <arashzarei.js@gmail.com>
2023-09-15 16:20:57 +0330

    added Advertisement component in homepage

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Banner from "@/modules/Banner";
 import Attributes from "@/modules/Attributes";
+import Advertisement from "@/modules/Advertisement";
 
 const HomePage = () => {
   return (
     <>
       <Banner />
       <Attributes />
+      <Advertisement />
     </>
   );
 };
