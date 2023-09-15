f6a11392973ad6a30b97d80cb8109e0f1132fe11
Arash Zarei <arashzarei.js@gmail.com>
2023-09-15 14:06:42 +0330

    responsived footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${yekan.className} max-w-6xl mx-auto`}>
+      <body className={`${yekan.className} max-w-6xl mx-auto px-2`}>
         <Layout>{children}</Layout>
       </body>
     </html>
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between p-4 bg-blue-700 rounded-md text-white mb-3">
+    <footer className="flex flex-col justify-end lg:flex-row items-center lg:justify-between gap-4 p-4 bg-blue-700 rounded-md text-white mb-3">
       <div className="max-w-3xl">
-        <h3 className="text-xl mb-4 text-black font-bold">سامانه خرید و اجاره ملک</h3>
+        <h3 className="text-xl mb-4 text-black font-bold">
+          سامانه خرید و اجاره ملک
+        </h3>
         <p>
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
           استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
           ستون و سطرآنچنان که لازم است.
         </p>
       </div>
-      <div>
-        <ul className="list-disc">
+      <div className="w-full lg:text-left lg:w-fit">
+        <ul className="list-none lg:list-disc">
           <li>تعرفه قانونی</li>
           <li>دسترسی سریع</li>
           <li>مشاورین خبره</li>
