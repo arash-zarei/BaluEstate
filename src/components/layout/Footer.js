91a8ac3e69487c6bf129bec4381df08af1379482
Arash Zarei <arashzarei.js@gmail.com>
2023-09-15 13:55:52 +0330

    added style in footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <h3>سامانه خرید و اجاره ملک</h3>
+    <footer className="flex items-center justify-between p-4 bg-blue-700 rounded-md text-white mb-3">
+      <div className="max-w-3xl">
+        <h3 className="text-xl mb-4 text-black font-bold">سامانه خرید و اجاره ملک</h3>
         <p>
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
           استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
@@ -12,7 +12,7 @@ const Footer = () => {
         </p>
       </div>
       <div>
-        <ul>
+        <ul className="list-disc">
           <li>تعرفه قانونی</li>
           <li>دسترسی سریع</li>
           <li>مشاورین خبره</li>
