eef2f462b3e57466666398c37bfcf531fce25dc1
Arash Zarei <arashzarei.js@gmail.com>
2023-09-15 23:14:59 +0330

    Authenticationed headr Component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,17 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { ThreeDots } from "react-loader-spinner";
+import { AiOutlineUser } from "react-icons/ai";
 
 // Icons
 import { FiLogIn } from "react-icons/fi";
 
 const Header = () => {
+  const { status } = useSession();
+
   return (
     <header className="w-full flex justify-between items-center p-4 my-3 rounded-md bg-blue-700">
       <div>
@@ -18,10 +25,32 @@ const Header = () => {
         </ul>
       </div>
       <div>
-        <Link href="signin" className="flex items-center gap-1 bg-white rounded-md px-2">
-          <FiLogIn />
-          <span>ورود</span>
-        </Link>
+        {status === "loading" && (
+          <ThreeDots
+            height="40"
+            width="40"
+            radius="9"
+            color="#facc15"
+            ariaLabel="three-dots-loading"
+          />
+        )}
+        {status === "unauthenticated" && (
+          <Link
+            href="/signin"
+            className="flex items-center gap-1 bg-white rounded-md px-2"
+          >
+            <FiLogIn />
+            <span>ورود</span>
+          </Link>
+        )}
+        {status === "authenticated" && (
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-1 bg-yellow-400 p-1 rounded-md px-2"
+          >
+            <AiOutlineUser size={25} />
+          </Link>
+        )}
       </div>
     </header>
   );
