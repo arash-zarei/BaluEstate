fedb4db7cadbc592df1a382444f5444f17823d59
Arash Zarei <arashzarei.js@gmail.com>
2023-09-16 13:52:03 +0330

    Securing the signup page

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,6 +1,13 @@
 import SignUpPage from "@/template/SignUpPage";
 
-const page = () => {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+
   return <SignUpPage />;
 };
 
