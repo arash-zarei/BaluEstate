87f1e52f3b74225c9de8c565b6bd281660b49232
Arash Zarei <arashzarei.js@gmail.com>
2023-09-18 14:27:25 +0330

    created model for ads

---
+++ src/models/Ads.js
@@ -0,0 +1,60 @@
+import { Schema, model, models } from "mongoose";
+
+const adsSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    realEstate: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    constructionDate: {
+      type: Date,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ["villa", "apartment", "store", "office"],
+      required: true,
+    },
+    amenities: {
+      type: [String],
+      default: [],
+    },
+    rules: {
+      type: [String],
+      default: [],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    // published: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+  },
+  { timestamps: true }
+);
+
+const Ads = models.Ads || model("Ads", adsSchema);
+
+export default Ads;
