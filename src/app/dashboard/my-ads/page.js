9c8950ed2d20dcdc75586d52fadd2ee0ff45c9cc
Arash Zarei <arashzarei.js@gmail.com>
2023-09-18 15:13:08 +0330

    get ads from DB

--- src/app/dashboard/my-ads/page.js
+++ src/app/dashboard/my-ads/page.js
@@ -1,7 +1,26 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import User from "@/models/User";
 import MyAdsPage from "@/template/MyAdsPage";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
 
-const page = () => {
-  return <MyAdsPage />;
+const page = async () => {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+
+  const [user] = await User.aggregate([
+    { $match: { email: session.user.email } },
+    {
+      $lookup: {
+        from: "ads",
+        foreignField: "userId",
+        localField: "_id",
+        as: "ads",
+      },
+    },
+  ]);
+
+  return <MyAdsPage ads={user.ads} />;
 };
 
 export default page;
