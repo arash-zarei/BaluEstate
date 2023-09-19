3735d7c07503b99ad2fcd628a7e1d92e63a8ac03
Arash Zarei <arashzarei.js@gmail.com>
2023-09-19 14:01:00 +0330

    changed component for reusable for edit and create ads

--- src/components/template/AddAdsPage.js
+++ src/components/template/AddAdsPage.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 import CustomDatePicker from "@/modules/AddAdsPage/CustomDatePicker";
@@ -8,8 +8,9 @@ import RadioList from "@/modules/AddAdsPage/RadioList";
 import TextInputs from "@/modules/AddAdsPage/TextInputs";
 import TextList from "@/modules/AddAdsPage/TextList";
 import { ProgressBar } from "react-loader-spinner";
+import { useRouter } from "next/navigation";
 
-const AddAdsPage = () => {
+const AddAdsPage = ({ data }) => {
   const [profileData, setProfileData] = useState({
     title: "",
     description: "",
@@ -24,6 +25,12 @@ const AddAdsPage = () => {
   });
 
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (data) setProfileData(data);
+  }, []);
+
+  const router = useRouter();
 
   const submitHandler = async () => {
     setLoading(true);
@@ -41,12 +48,35 @@ const AddAdsPage = () => {
       toast.error(data.error);
     } else {
       toast.success(data.message);
+      router.refresh();
+    }
+  };
+
+  const editHandler = async () => {
+    setLoading(true);
+
+    const res = await fetch("/api/ads", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(profileData),
+    });
+    const data = await res.json();
+
+    setLoading(false);
+
+    if (data.error) {
+      toast.error(data.error);
+    } else {
+      toast.success(data.message);
+      router.refresh();
     }
   };
 
   return (
     <div className="w-full mb-7" id="ads">
-      <h3 className="py-2 rounded-md bg-yellow-400 px-4">ثبت آگهی جدید</h3>
+      <h3 className="py-2 rounded-md bg-yellow-400 px-4">
+        {data ? "ویرایش آگهی" : "ثبت آگهی جدید"}
+      </h3>
       <div className="mt-5">
         <TextInputs
           title="عنوان آگهی"
@@ -114,6 +144,13 @@ const AddAdsPage = () => {
             barColor="#facc15"
             wrapperStyle={{ margin: "0 auto" }}
           />
+        ) : data ? (
+          <button
+            onClick={editHandler}
+            className="bg-blue-700 w-full py-2 rounded-md text-white font-semibold my-10"
+          >
+            ویرایش آگهی
+          </button>
         ) : (
           <button
             onClick={submitHandler}
