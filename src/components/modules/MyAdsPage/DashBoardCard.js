2393212816432f8bae8a003271fb48297bb1590f
Arash Zarei <arashzarei.js@gmail.com>
2023-09-19 13:41:24 +0330

    create function for move to edit page

--- src/components/modules/MyAdsPage/DashBoardCard.js
+++ src/components/modules/MyAdsPage/DashBoardCard.js
@@ -1,14 +1,24 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React from "react";
 import Card from "@/modules/MyAdsPage/Card";
 
 const DashBoardCard = ({ data }) => {
+  const router = useRouter();
+
+  const editHandler = () => {
+    router.push(`/dashboard/my-ads/${data._id}`);
+  };
+
   return (
     <div className="flex flex-col items-center border-2 border-blue-600 py-5 px-8 rounded-lg">
       <Card data={data} />
       <div className="flex items-center gap-10 my-4">
-        <button className="py-1 px-3 rounded-md font-semibold bg-yellow-400">
+        <button
+          onClick={editHandler}
+          className="py-1 px-3 rounded-md font-semibold bg-yellow-400"
+        >
           ویرایش
         </button>
         <button className="py-1 px-3 rounded-md font-semibold bg-red-600 text-white">
