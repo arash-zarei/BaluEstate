17efdb836bd37e29a6840a1c6415ac72e3605446
Arash Zarei <arashzarei.js@gmail.com>
2023-09-20 15:12:12 +0330

    get data and pass to component

--- src/app/advertisements/page.js
+++ src/app/advertisements/page.js
@@ -1,7 +1,20 @@
 import AdvertisementsPage from "@/template/AdvertisementsPage";
 
-const Advertisements = () => {
-  return <AdvertisementsPage />;
+const Advertisements = async () => {
+  // بهتر است در کامپوننت های سرورساید از ای پی آی روت استفاده نکنیم(این مورد حالت تمرینی دارد)
+  const res = await fetch("http://localhost:3000/api/ads", {
+    cache: "no-store",
+  });
+  const data = await res.json();
+
+  if (data.error)
+    return (
+      <h3 className="text-xl py-2 px-3 rounded-md bg-red-200 text-red-700">
+        {data.error}
+      </h3>
+    );
+
+  return <AdvertisementsPage data={data} />;
 };
 
 export default Advertisements;
