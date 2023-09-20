09016e33a686afbb318d8bc05571c87d44c5116f
Arash Zarei <arashzarei.js@gmail.com>
2023-09-20 16:06:29 +0330

    added href for categories

--- src/components/modules/HomePage/CategoryCard.js
+++ src/components/modules/HomePage/CategoryCard.js
@@ -5,7 +5,7 @@ import React from "react";
 const CategoryCard = ({ name, title }) => {
   return (
     <div className="m-4 bg-white shadow-xl shadow-blue-200 rounded-md py-3 px-4 flex justify-center transition-all hover:rotate-[-15deg] hover:scale-[1.09]">
-      <Link href="/" className="flex flex-col items-center gap-6">
+      <Link href={`/advertisements?category=${name}`} className="flex flex-col items-center gap-6">
         <Image
           src={`/images/${name}.png`}
           alt={title}
