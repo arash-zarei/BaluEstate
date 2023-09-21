a8215bae99b02d75c4af50f826501bfdc4acfd5c
Arash Zarei <arashzarei.js@gmail.com>
2023-09-21 11:52:17 +0330

    get role User

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,21 +1,25 @@
 import DashboardLayouts from "@/layout/DashboardLayout";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
 
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const DashboardLayout = async ({ children }) => {
-
   const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/signup");
   }
 
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+
   return (
     <div className="w-full flex flex-col md:flex-row gap-3 mt-10">
       <div className="w-full md:w-[20%]">
-        <DashboardLayouts />
+        <DashboardLayouts role={user.role} email={user.email} />
       </div>
       <div className="w-full my-10 md:w-[80%] md:my-0">{children}</div>
     </div>
