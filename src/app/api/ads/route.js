8bf55d2ce8af42e30007f38b6dc25c2c226c0a3f
Arash Zarei <arashzarei.js@gmail.com>
2023-09-21 11:37:59 +0330

    get ads by published's true

--- src/app/api/ads/route.js
+++ src/app/api/ads/route.js
@@ -10,7 +10,7 @@ export async function GET() {
   try {
     await connectDB();
 
-    const ads = await Ads.find().select("-userId");
+    const ads = await Ads.find({ published: true }).select("-userId");
 
     return NextResponse.json(
       {
