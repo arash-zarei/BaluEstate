27ea6c454d0c4d06a04f85be5c1db12382602c87
Arash Zarei <arashzarei.js@gmail.com>
2023-09-21 12:25:52 +0330

    added item for admin role

--- src/components/layout/DashboardLayout.js
+++ src/components/layout/DashboardLayout.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/api/auth/[...nextauth]/route"
 import Link from "next/link";
 import ButtonSignOut from "@/modules/LayoutDashboard/ButtonSignOut";
 
@@ -10,18 +8,20 @@ import { CgProfile } from "react-icons/cg";
 import { AiOutlineUser } from "react-icons/ai";
 import { PiFlagBanner } from "react-icons/pi";
 import { BiMessageSquareAdd } from "react-icons/bi";
+import { GrUserAdmin } from "react-icons/gr";
 
-const DashboardLayouts = async () => {
-  const session = await getServerSession(authOptions);
-
+const DashboardLayouts = async ({ role, email }) => {
   return (
     <div className="w-full shadow-lg shadow-gray-500 rounded-md py-5 px-4">
       <div className="w-full flex flex-col items-center gap-4 justify-center border-b-2 pb-3">
         <span className="w-[40px] h-[40px] flex justify-center items-center rounded-full bg-blue-500">
           <AiOutlineUser size={25} />
         </span>
+        <p className="text-xl font-semibold">
+          {role === "ADMIN" ? "مدیر" : null}
+        </p>
         <p className="w-full overflow-x-auto text-center text-gray-500 font-semibold">
-          {session?.user.email}
+          {email}
         </p>
       </div>
       <div>
@@ -38,6 +38,12 @@ const DashboardLayouts = async () => {
             <BiMessageSquareAdd />
             <Link href="/dashboard/add">ثبت آگهی</Link>
           </li>
+          {role === "ADMIN" ? (
+            <li className="flex items-center gap-2 mt-5">
+              <GrUserAdmin />
+              <Link href="/admin">در انتظار تایید</Link>
+            </li>
+          ) : null}
         </ul>
       </div>
       <ButtonSignOut />
